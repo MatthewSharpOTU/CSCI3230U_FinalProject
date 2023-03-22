347b3d6d4516c38c1b70332eb03db1071a81ea07
Jaelen Wright <jaelen.wright@ontariotechu.net>
2023-03-21 23:59:05 -0400

    Updated files
    does not include text or logo yet

--- Final Project/Scripts/signIn.js
+++ Final Project/Scripts/signIn.js
@@ -1,13 +1,16 @@
+//global variables
 var curUser;
 
 function getUser(){
-    var username = document.querySelector('#username');
-    var password = document.querySelector('#password');
+    var username = document.querySelector('#username').value;
+    var password = document.querySelector('#password').value;
 
-    console.log(username.value);
-    console.log(password.value);
-    if (username.value=="admin"&&password.value=="admin"){
-        curUser = username.value;
+    console.log(username);
+    console.log(password);
+    //calls login function to check if user and password match records
+    //login(username,password);
+    if (username=="admin"&&password=="admin"){
+        curUser = username;
         document.location.href = "home.html";
         //make link to homepage or profile page afterwards
     }
