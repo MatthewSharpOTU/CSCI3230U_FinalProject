f972be918fba1357f36a689a69678a9042459994
xGarry <46723163+xGarry@users.noreply.github.com>
2023-04-05 15:16:42 -0400

    Music player added

---
+++ Profile/data.js
@@ -0,0 +1,26 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'music/track 1.mp3',
+        artist: 'artist 1',
+        cover: 'images/bg.png'
+    },
+    {
+        name: 'song 2',
+        path: 'music/track 2.mp3',
+        artist: 'artist 2',
+        cover: 'images/bg.png'
+    },
+    {
+        name: 'song 3',
+        path: 'music/track 3.mp3',
+        artist: 'artist 3',
+        cover: 'images/bg.png'
+    },
+    {
+        name: 'song 4',
+        path: 'music/track 4.mp3',
+        artist: 'artist 4',
+        cover: 'images/bg.png'
+    }
+]
--- Profile/scripts/Profile.js
+++ Profile/scripts/Profile.js
@@ -1,41 +1,89 @@
 const audio = document.getElementById("audio-player");
 const playButton = document.getElementById("play-button");
 const pauseButton = document.getElementById("pause-button");
-const backButton = document.getElementById("back-button");
+const backButton = document.getElementById("backward-button");
 const forwardButton = document.getElementById("forward-button");
 const progress = document.getElementById("progress");
+const homeButton = document.querySelector("#home-icon");
+const title = document.querySelector(".title");
+const artist = document.querySelector(".artist");
+const disk = document.querySelector(".song-image");
+const currentTime = document.querySelector(".current-time");
+const songDuration = document.querySelector(".song-duration");
+
 let isPlaying = false;
+let currentMusic = 0;
+
+const setMusic = (i) => {
+  let song = songs[i];
+  currentMusic = i;
+  audio.src = song.path;
+  title.innerHTML = song.name;
+  artist.innerHTML = song.artist;
+  disk.style.backgroundImage = `url('${song.cover}')`;
+
+  currentTime.innerHTML = "00:00";
+  setTimeout(() => {
+    songDuration.innerHTML = formatTime(audio.duration);
+  }, 300);
+}
+
+setMusic(0);
+
+const formatTime = (time) => {
+  let min = Math.floor(time/60);
+  if(min<10){
+    min=`0${min}`;
+  }
+  let sec = Math.floor(time % 60);
+  if(sec < 10){
+    sec = `0${sec}`;
+  }
+  return `${min} : ${sec}`;
+}
 
 function togglePlay() {
   if (isPlaying) {
     audio.pause();
     isPlaying = false;
-    playButton.style.display = "block";
-    pauseButton.style.display = "none";
+
   } else {
     audio.play();
     isPlaying = true;
-    playButton.style.display = "none";
-    pauseButton.style.display = "block";
+
   }
-}
-
-function back() {
-  audio.currentTime -= 10;
-}
-
-function forward() {
-  audio.currentTime += 10;
 }
 
 function updateProgress() {
   const percentage = (audio.currentTime / audio.duration) * 100;
   progress.style.width = `${percentage}%`;
+  songDuration.innerHTML = formatTime(audio.duration - audio.currentTime);
+  currentTime.innerHTML = formatTime(audio.currentTime);
+
 }
 
-audio.addEventListener("timeupdate", updateProgress);
+homeButton.addEventListener("click", () =>{
+  location.href = '../Homepage/Homepage.html';
+});
 
+audio.addEventListener("timeupdate", updateProgress);
 playButton.addEventListener("click", togglePlay);
-pauseButton.addEventListener("click", togglePlay);
-backButton.addEventListener("click", back);
-forwardButton.addEventListener("click", forward);
+
+backButton.addEventListener("click",() => {
+  if(currentMusic <= 0){
+   currentMusic = songs.length - 1;
+  }else{
+   currentMusic--;
+  }
+  setMusic(currentMusic);
+  audio.play;
+ });
+forwardButton.addEventListener("click", () => {
+ if(currentMusic >= songs.length - 1){
+  currentMusic = 0;
+ }else{
+  currentMusic++;
+ }
+ setMusic(currentMusic);
+ audio.play;
+});
