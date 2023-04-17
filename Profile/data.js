9aa06f1543510cb7040b23b0de58972aac5304ea
xGarry <46723163+xGarry@users.noreply.github.com>
2023-04-17 18:32:56 -0400

    Updates

--- Profile/data.js
+++ Profile/data.js
@@ -1,30 +1,59 @@
 let songs = [
     {
-        id: '1',
+        id: '0',
         name: 'the BLACK seminole',
         path: 'music/01. the BLACK seminole.mp3',
         artist: 'Lil Yachty',
         cover: 'images/Lil Yachty - Let’s Start Here.jpg'
     },
     {
-        id: '2',
+        id: '1',
         name: 'the ride-',
         path: 'music/02. the ride-.mp3',
         artist: 'Lil Yachty',
         cover: 'images/Lil Yachty - Let’s Start Here.jpg'
     },
     {
-        id: '3',
+        id: '2',
         name: 'running out of time',
         path: 'music/03. running out of time.mp3',
         artist: 'Lil Yachty',
         cover: 'images/Lil Yachty - Let’s Start Here.jpg'
     },
     {
-        id: '4',
+        id: '3',
         name: 'pRETTy',
         path: 'music/04. pRETTy.mp3',
         artist: 'Lil Yachty',
         cover: 'images/Lil Yachty - Let’s Start Here.jpg'
+    },
+    {
+        id: '4',
+        name: '_(failure(_',
+        path: 'music/05. _(failure(_.mp3',
+        artist: 'Lil Yachty',
+        cover: 'images/Lil Yachty - Let’s Start Here.jpg'
+    },
+    {
+        id: '5',
+        name: 'THE zone~',
+        path: 'music/06. THE zone~.mp3',
+        artist: 'Lil Yachty',
+        cover: 'images/Lil Yachty - Let’s Start Here.jpg'
+    }
+]
+
+let playlists = [
+    {
+        name: 'Playlist #1',
+        tracks: ['1', '2', '3', '4', '5'],
+    },
+    {
+        name: 'Playlist #2',
+        tracks: ['1', '2', '3'],
+    },
+    {
+        name: 'Playlist #3',
+        tracks: ['1', '2', '3', '4'],
     }
 ]
--- Profile/scripts/Profile.js
+++ Profile/scripts/Profile.js
@@ -4,7 +4,7 @@ const pauseButton = document.getElementById("pause-button");
 const backButton = document.getElementById("backward-button");
 const forwardButton = document.getElementById("forward-button");
 const progress = document.getElementById("song-progress");
-const homeButton = document.querySelector("#home-icon");
+const homeButton = document.querySelector(".logo");
 const title = document.querySelector(".title");
 const artist = document.querySelector(".artist");
 const disk = document.querySelector(".song-image");
@@ -14,12 +14,14 @@ const volume = document.getElementById("volume");
 const volumeIcon = document.getElementById("volume-icon");
 const shuffle = document.getElementById("shuffle-button");
 const repeat = document.getElementById("repeat-button");
+const addSong = document.getElementById("add-song");
 
 let isPlaying = false;
 let currentMusic = 0;
 volume.value = 100;
 shuffleStatus = false;
 repeatStatus = false;
+
 
 const setMusic = (i) => {
   let song = songs[i];
@@ -28,6 +30,7 @@ const setMusic = (i) => {
   title.innerHTML = song.name;
   artist.innerHTML = song.artist;
   disk.style.backgroundImage = `url('${song.cover}')`;
+  document.getElementById("song-id").setAttribute("value", `${currentMusic}`);
 
   currentTime.innerHTML = "00:00";
   setTimeout(() => {
@@ -36,6 +39,28 @@ const setMusic = (i) => {
 }
 
 setMusic(0);
+
+function addToPlaylist(x, y){
+  let newSong = document.createElement("li");
+  newSong.classList.add('song');
+  newSong.innerHTML = 
+  `
+  <span>0${y}</span>
+  <img src="${songs[x].cover}" alt="Alan">
+  <h5>
+    ${songs[x].name}
+      <div class="subtitle">${songs[x].artist}</div>
+  </h5>
+      <i class="fas fa-play-circle play_now" id="${x}"></i>`
+
+  document.getElementsByClassName("songs")[0].appendChild(newSong);
+}
+
+
+
+for(var i=0; i<playlists[0].tracks.length; i++){
+  addToPlaylist(playlists[0].tracks[i], i+1);
+}
 
 const formatTime = (time) => {
   let min = Math.floor(time/60);
@@ -121,6 +146,66 @@ repeat.addEventListener("click", ()=>{
   }
 });
 
+//add song to playlist
+let selectedSong = 0;
+let add = false;
+addSong.addEventListener("click", () => {
+  add = true;
+  //console.log(document.getElementById("song-id").getAttribute("value"));
+  selectedSong = document.getElementById("song-id").getAttribute("value");
+});
+
+let currentPlaylist = 0;
+document.querySelectorAll('.playlist').forEach(item => {
+  item.addEventListener("click", ()=>{
+    
+      if(add===true){
+        for(var z in playlists){
+          //console.log(i);
+          if(playlists[z].name === item.textContent){
+            console.log(z);
+            playlists[z].tracks.push(selectedSong);
+            if(z === currentPlaylist){
+              console.log(z);
+              addToPlaylist(selectedSong, playlists[z].tracks.length);
+            }
+          }
+        }
+        add=false;
+      } else {
+        document.querySelector(".songs").innerHTML= "";
+        for(var i in playlists){
+          if(playlists[i].name === item.textContent){
+              currentPlaylist = i;
+              console.log(i);
+              for(var j=0; j<playlists[i].tracks.length; j++){
+                addToPlaylist(playlists[i].tracks[j], j+1);
+              }
+              break; 
+          }
+      }
+      }
+    })
+})
+
+let createPlaylist = document.getElementById("playlist-add");
+
+createPlaylist.addEventListener("click",() => {
+  playlists.push({
+    name: `Playlist #${playlists.length+1}`,
+    tracks: ['1'],
+  })
+
+  let newPlaylist = document.createElement("h4");
+  newPlaylist.classList.add('playlist');
+  newPlaylist.innerHTML = `<span></span>Playlist #${playlists.length}`;
+
+  document.getElementsByClassName("playlists")[0].appendChild(newPlaylist);
+
+  var x = 
+  console.log(playlists);
+})
+
 backButton.addEventListener("click",() => {
   if(currentMusic <= 0){
    currentMusic = songs.length - 1;
@@ -136,6 +221,7 @@ forwardButton.addEventListener("click", () => {
     currentMusic = 0;
   if(shuffleStatus === true){
     currentMusic = Math.random() * 4;
+    console.log()
   }
   }else{
     currentMusic++;
@@ -150,8 +236,44 @@ audio.addEventListener('ended', function(){
   audio.play;
 });
 
-Array.from(document.getElementsByClassName('song')).forEach((element, i)=>{
-  element.getElementsByTagName('img')[0].src = songs[i].cover;
-  element.getElementsByTagName('h5')[0].innerHTML = songs[i].name;
-  element.getElementsByTagName('div')[0].innerHTML = songs[i].name;
+const edit = document.getElementById("edit");
+edit.addEventListener("click", () => {
+  let newName = prompt("Rename your playlist:", `${playlists[currentPlaylist].name}`);
+
+  if (newName != null) {
+    document.querySelector(".playlists").children.item(currentPlaylist).innerHTML = `<span></span>${newName}`;
+    playlists[currentPlaylist].name = newName;
+  }
+})
+
+const deletePlaylist = document.getElementById("delete");
+deletePlaylist.addEventListener("click", () => {
+  document.querySelector(".playlists").children.item(currentPlaylist).innerHTML = "";
+  playlists[currentPlaylist].remove;
+  
+});
+
+playSong = document.querySelectorAll(".play_now").forEach(playSong => {
+  playSong.addEventListener("click", ()=>{
+    currentMusic = parseInt(playSong.getAttribute("id"));
+    setMusic(currentMusic);
+    audio.play;
+  })
+})
+
+editUser = document.getElementById("edit-user");
+editUser.addEventListener("click", () => {
+  if(editUser.classList.value === "fas fa-user-edit"){
+    document.getElementById("user-name").setAttribute("contenteditable", "true");
+    document.getElementById("user-motto").setAttribute("contenteditable", "true");
+    document.getElementById("user-bio").setAttribute("contenteditable", "true");
+    editUser.classList.remove("fa-user-edit");
+    editUser.classList.add("fa-save");
+  }else{
+    document.getElementById("user-name").setAttribute("contenteditable", "false");
+    document.getElementById("user-motto").setAttribute("contenteditable", "false");
+    document.getElementById("user-bio").setAttribute("contenteditable", "false");
+    editUser.classList.remove("fa-save");
+    editUser.classList.add("fa-user-edit");
+  }
 })
