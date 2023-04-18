2b05281bda223da5528f2e528bdab732e3aee53a
xGarry <46723163+xGarry@users.noreply.github.com>
2023-04-18 14:57:34 -0400

    Clean up code + ui fixes

--- Profile/scripts/Profile.js
+++ Profile/scripts/Profile.js
@@ -1,3 +1,4 @@
+//Store relevant HTML elements
 const audio = document.getElementById("audio-player");
 const playButton = document.getElementById("play-button");
 const pauseButton = document.getElementById("pause-button");
@@ -7,7 +8,7 @@ const progress = document.getElementById("song-progress");
 const homeButton = document.querySelector(".logo");
 const title = document.querySelector(".title");
 const artist = document.querySelector(".artist");
-const disk = document.querySelector(".song-image");
+const cover = document.querySelector(".song-image");
 const currentTime = document.querySelector(".current-time");
 const songDuration = document.querySelector(".song-duration");
 const volume = document.getElementById("volume");
@@ -15,53 +16,58 @@ const volumeIcon = document.getElementById("volume-icon");
 const shuffle = document.getElementById("shuffle-button");
 const repeat = document.getElementById("repeat-button");
 const addSong = document.getElementById("add-song");
+let createPlaylist = document.getElementById("playlist-add");
 
+//Initialize player variables
 let isPlaying = false;
-let currentMusic = 0;
+let currentSong = 0;
 volume.value = 100;
 shuffleStatus = false;
-repeatStatus = false;
 
-
-const setMusic = (i) => {
+//Play selected song
+function playSong(i){
   let song = songs[i];
-  currentMusic = i;
+  currentSong = i;
   audio.src = song.path;
   title.innerHTML = song.name;
   artist.innerHTML = song.artist;
-  disk.style.backgroundImage = `url('${song.cover}')`;
-  document.getElementById("song-id").setAttribute("value", `${currentMusic}`);
+  cover.style.backgroundImage = `url('${song.cover}')`;
+  document.getElementById("song-id").setAttribute("value", `${currentSong}`);
 
   currentTime.innerHTML = "00:00";
   setTimeout(() => {
     songDuration.innerHTML = formatTime(audio.duration);
   }, 300);
 }
+//initialize player
+playSong(0);
 
-setMusic(0);
-
+//Add new song's UI elements to selected playlist
 function addToPlaylist(x, y){
   let newSong = document.createElement("li");
   newSong.classList.add('song');
   newSong.innerHTML = 
   `
   <span>0${y}</span>
-  <img src="${songs[x].cover}" alt="Alan">
+  <img src="${songs[x].cover}">
   <h5>
     ${songs[x].name}
-      <div class="subtitle">${songs[x].artist}</div>
+      <div class="artist-name">${songs[x].artist}</div>
   </h5>
       <i class="fas fa-play-circle play_now" id="${x}"></i>`
 
   document.getElementsByClassName("songs")[0].appendChild(newSong);
 }
 
-
-
-for(var i=0; i<playlists[0].tracks.length; i++){
-  addToPlaylist(playlists[0].tracks[i], i+1);
+//initialize playlist
+function initializePlaylist(){
+  for(var i=0; i<playlists[0].tracks.length; i++){
+    addToPlaylist(playlists[0].tracks[i], i+1);
+  }
 }
+initializePlaylist()
 
+//correctly format song duration
 const formatTime = (time) => {
   let min = Math.floor(time/60);
   if(min<10){
@@ -74,6 +80,7 @@ const formatTime = (time) => {
   return `${min} : ${sec}`;
 }
 
+//Handle play/pause button
 function togglePlay() {
   if (isPlaying) {
     audio.pause();
@@ -88,16 +95,21 @@ function togglePlay() {
     playButton.classList.remove("fa-play");
   }
 }
+playButton.addEventListener("click", togglePlay);
 
+//Update song progress bar while playing
 setInterval(()=>{
   progress.value = (audio.currentTime/audio.duration)*100;
 },500);
 
+//Update time stamps
 function updateProgress() {
   songDuration.innerHTML = formatTime(audio.duration - audio.currentTime);
   currentTime.innerHTML = formatTime(audio.currentTime);
 }
+audio.addEventListener("timeupdate", updateProgress);
 
+//Update currentTime of song when user interacts with progress slider
 progress.onchange = function(){
   audio.play;
   audio.currentTime = (progress.value/100)*audio.duration;
@@ -105,6 +117,7 @@ progress.onchange = function(){
   playButton.classList.remove("fa-play");
 }
 
+//Update volume level when volume slider is changed
 volume.onchange = function(){
   audio.volume = volume.value/100;
   console.log(volume.value);
@@ -117,13 +130,12 @@ volume.onchange = function(){
   }
 }
 
+//Take user to homepage when logo icon is clicked
 homeButton.addEventListener("click", () =>{
   location.href = '../Homepage/Homepage.html';
 });
 
-audio.addEventListener("timeupdate", updateProgress);
-playButton.addEventListener("click", togglePlay);
-
+//Enable or disable shuffle feature
 shuffle.addEventListener("click", ()=>{
   if(shuffleStatus === false){
     shuffleStatus = true;
@@ -134,108 +146,105 @@ shuffle.addEventListener("click", ()=>{
   }
 });
 
+//Enable or disable repeat feature
 repeat.addEventListener("click", ()=>{
-  if(repeatStatus === false){
-    repeatStatus = true;
+  if(audio.loop === false){
     $("#repeat-button").css("color", "#f53192");
     audio.loop = true;
   }else{
-    repeatStatus = false;
     $("#repeat-button").css("color", "black");
     audio.loop = false;
   }
 });
 
-//add song to playlist
+//Event listener for plus button on player for adding to playlists
 let selectedSong = 0;
 let add = false;
 addSong.addEventListener("click", () => {
   add = true;
-  //console.log(document.getElementById("song-id").getAttribute("value"));
   selectedSong = document.getElementById("song-id").getAttribute("value");
 });
 
+//Adds song to selected playlist if add === true
 let currentPlaylist = 0;
 document.querySelectorAll('.playlist').forEach(item => {
-  item.addEventListener("click", ()=>{
-    
-      if(add===true){
-        for(var z in playlists){
-          //console.log(i);
-          if(playlists[z].name === item.textContent){
+  item.addEventListener("click", ()=>{  
+    if(add===true){
+      for(var z in playlists){
+        if(playlists[z].name === item.textContent){
+          console.log(z);
+          playlists[z].tracks.push(selectedSong);
+
+          if(z === currentPlaylist){
             console.log(z);
-            playlists[z].tracks.push(selectedSong);
-            if(z === currentPlaylist){
-              console.log(z);
-              addToPlaylist(selectedSong, playlists[z].tracks.length);
-            }
+            addToPlaylist(selectedSong, playlists[z].tracks.length);
           }
         }
-        add=false;
-      } else {
-        document.querySelector(".songs").innerHTML= "";
-        for(var i in playlists){
-          if(playlists[i].name === item.textContent){
-              currentPlaylist = i;
-              console.log(i);
-              for(var j=0; j<playlists[i].tracks.length; j++){
-                addToPlaylist(playlists[i].tracks[j], j+1);
-              }
-              break; 
-          }
       }
+    add=false;
+    } else {
+      document.querySelector(".songs").innerHTML= "";
+      for(var i in playlists){
+        if(playlists[i].name === item.textContent){
+            currentPlaylist = i;
+            console.log(i);
+            for(var j=0; j<playlists[i].tracks.length; j++){
+              addToPlaylist(playlists[i].tracks[j], j+1);
+            }
+          break; 
+        }
       }
-    })
-})
+    }
+  });
+});
 
-let createPlaylist = document.getElementById("playlist-add");
-
+//Event listener for plus button in sidebar for creating new playlists
 createPlaylist.addEventListener("click",() => {
   playlists.push({
     name: `Playlist #${playlists.length+1}`,
     tracks: ['1'],
   })
-
   let newPlaylist = document.createElement("h4");
   newPlaylist.classList.add('playlist');
   newPlaylist.innerHTML = `<span></span>Playlist #${playlists.length}`;
-
   document.getElementsByClassName("playlists")[0].appendChild(newPlaylist);
-
-  var x = 
   console.log(playlists);
 })
 
+//Go to previous track
 backButton.addEventListener("click",() => {
-  if(currentMusic <= 0){
-   currentMusic = songs.length - 1;
+  if(currentSong <= 0){
+   currentSong = songs.length - 1;
   }else{
-   currentMusic--;
+   currentSong--;
   }
-  setMusic(currentMusic);
+  playSong(currentSong);
   audio.play;
  });
 
+ //Go to next track
 forwardButton.addEventListener("click", () => {
-  if(currentMusic >= songs.length - 1){
-    currentMusic = 0;
+  if(currentSong >= songs.length - 1){
+    currentSong = 0;
   if(shuffleStatus === true){
-    currentMusic = Math.random() * 4;
+    currentSong = Math.random() * 4;
     console.log()
   }
   }else{
-    currentMusic++;
+    currentSong++;
   }
-  setMusic(currentMusic);
+  playSong(currentSong);
   audio.play;
 });
 
+//Play next song when current song ends
 audio.addEventListener('ended', function(){
-  currentMusic++;
-  setMusic(currentMusic);
+  currentSong++;
+  playSong(currentSong);
   audio.play;
 });
 
+//Rename playlists
 const edit = document.getElementById("edit");
 edit.addEventListener("click", () => {
   let newName = prompt("Rename your playlist:", `${playlists[currentPlaylist].name}`);
@@ -246,6 +255,7 @@ edit.addEventListener("click", () => {
   }
 })
 
+//Delete selected playlist
 const deletePlaylist = document.getElementById("delete");
 deletePlaylist.addEventListener("click", () => {
   document.querySelector(".playlists").children.item(currentPlaylist).innerHTML = "";
@@ -253,14 +263,16 @@ deletePlaylist.addEventListener("click", () => {
   
 });
 
+//Play selected song in playlist
 playSong = document.querySelectorAll(".play_now").forEach(playSong => {
   playSong.addEventListener("click", ()=>{
-    currentMusic = parseInt(playSong.getAttribute("id"));
-    setMusic(currentMusic);
+    currentSong = parseInt(playSong.getAttribute("id"));
+    playSong(currentSong);
     audio.play;
   })
 })
 
+//Edit user information (display name, motto, bio)
 editUser = document.getElementById("edit-user");
 editUser.addEventListener("click", () => {
   if(editUser.classList.value === "fas fa-user-edit"){
